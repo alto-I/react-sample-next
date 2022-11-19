4420d584b422cb470a4468e8c6b2bdb9311f81cb
ito_arata <blmaldiy@gmail.com>
2022-11-19 11:16:28 +0900

    レンダリング関数をReact18に合わせる

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
+import { createRoot } from 'react-dom/client';
 import Hello from './components/Hello';
 import Name from './components/Name';
+import Message from './components/Message';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+console.log(root)
+
+root.render(
   <React.StrictMode>
     <Hello />
     <Name />
+    <Message />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
