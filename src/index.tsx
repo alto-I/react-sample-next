b2d47ba1bb321772e8ff19d028d399059481f2ed
ito_arata <blmaldiy@gmail.com>
2022-11-19 16:06:53 +0900

    3.5.2まで

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Parent } from './components/Parent';
+import { UseMemoSample } from './components/UseMemoSample';
 import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
@@ -8,7 +8,7 @@ const root = createRoot(container!);
 
 root.render(
   <React.StrictMode>
-    <Parent />
+    <UseMemoSample />
   </React.StrictMode>,
 );
 
