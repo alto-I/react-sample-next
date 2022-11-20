ca4c296708bed7d8eaedd011bd8ce8d9a16263c1
ito_arata <blmaldiy@gmail.com>
2022-11-20 20:41:24 +0900

    3.5.5まで

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { UseContextSample } from './components/UseContextSample';
+import { UseImperativeHandleSample } from './components/UseImperativeHandleSample';
 import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
@@ -8,7 +8,7 @@ const root = createRoot(container!);
 
 root.render(
   <React.StrictMode>
-    <UseContextSample />
+    <UseImperativeHandleSample />
   </React.StrictMode>,
 );
 
